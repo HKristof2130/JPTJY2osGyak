caaaad4241bd33cdcbf6ed18e7b1ebacb283fb3f
HKristof2130 <78855300+HKristof2130@users.noreply.github.com>
2021-03-16 16:07:31 +0100

    Create child.1.c

---
+++ JPTJY2_0310/child.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+
+int main()
+{
+     for(int i=0;i<5;i++){
+        printf("Hegyesi Kristof JPTJY2\n");
+        }
+
+    return 0;
+}
