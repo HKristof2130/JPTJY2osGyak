d2b0a27fc9adc44b60fa99a087c798b01b33e568
HKristof2130 <kmhegyesi1@gmail.com>
2021-04-27 21:21:21 +0200

    Update msgcreate_JPTJY2.c

--- JPTJY2_0421/msgcreate_JPTJY2.c
+++ JPTJY2_0421/msgcreate_JPTJY2.c
@@ -8,16 +8,16 @@
 struct msgbuf1 {		
 	long mtype;
 	char mtext[512];
-} sndbuf, *msgp;		/* message buffer es pointere */
+} sndbuf, *msgp;		
 
 
 
 main()
 {
-	int msgid;		/* uzenetsor azonosito */
-	key_t key;		/* kulcs az uzenetsorhoz */
-	int msgflg;		/* flag a kreaciohoz, uzenetkuldeshez */
-	int rtn, msgsz;		/* return es meret */
+	int msgid;		
+	key_t key;		
+	int msgflg;		
+	int rtn, msgsz;		
 	
 	key = MSGKEY;
 	msgflg = 00666 | IPC_CREAT;
@@ -29,9 +29,9 @@ main()
 	printf("\n Az msgid %d, %x : ", msgid,msgid);
 
 	msgp        = &sndbuf;
-	msgp->mtype = 1;   			/* tipus = text */
+	msgp->mtype = 1;   			
  	strcpy(msgp->mtext,"Egyik uzenet");
-	msgsz       = strlen(msgp->mtext) + 1;	/* az uzenet hossza */
+	msgsz       = strlen(msgp->mtext) + 1;	
 	/* es elkuldom: */
         rtn = msgsnd(msgid,(struct msgbuf *) msgp, msgsz, msgflg);
 	printf("\n Az 1. msgsnd visszaadott %d-t", rtn);
